428606ca89ada2284c053d47e754fe1ed30e3a3c
dennis guye <dennis@aion.network>
2019-10-02 15:57:15 -0400

    Version Bump

--- aion-etl/src/main/java/aion/dashboard/config/BuildVersion.java
+++ aion-etl/src/main/java/aion/dashboard/config/BuildVersion.java
@@ -7,10 +7,10 @@ package aion.dashboard.config;
 public class BuildVersion {
 	public static final String MAVEN_GROUP = "aion-etl";
 	public static final String MAVEN_NAME = "aion-etl";
-	public static final String VERSION = "2.3-59756ca-0527-26-09-2019";
-	public static final int GIT_REVISION = 101;
-	public static final String GIT_SHA = "59756cac72491655c57224f08406ab69b0edd731";
-	public static final String GIT_DATE = "2019-09-20T14:38:59Z";
-	public static final String BUILD_DATE = "2019-09-26T17:27:46Z";
-	public static final long BUILD_UNIX_TIME = 1569518866615L;
+	public static final String VERSION = "2.4-4b4ded4-0756-02-10-2019";
+	public static final int GIT_REVISION = 110;
+	public static final String GIT_SHA = "4b4ded4907c5dccc5122fe4eadcc31b5e88a8f68";
+	public static final String GIT_DATE = "2019-10-02T19:50:20Z";
+	public static final String BUILD_DATE = "2019-10-02T19:56:52Z";
+	public static final long BUILD_UNIX_TIME = 1570046212590L;
 }
