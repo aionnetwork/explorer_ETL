137c37e50ddba48b21c1a3736b59e64dde8f230c
Jesus Redon <jesus@movesfinancial.com>
2021-09-02 13:16:29 -0400

    Field table update

--- aion-etl/src/main/java/aion/dashboard/config/BuildVersion.java
+++ aion-etl/src/main/java/aion/dashboard/config/BuildVersion.java
@@ -7,10 +7,10 @@ package aion.dashboard.config;
 public class BuildVersion {
 	public static final String MAVEN_GROUP = "aion-etl";
 	public static final String MAVEN_NAME = "aion-etl";
-	public static final String VERSION = "2.4-3196c0b-0253-01-09-2021";
-	public static final int GIT_REVISION = 129;
-	public static final String GIT_SHA = "3196c0ba53d76a3dbf52d42cc4a539a713db3b2e";
-	public static final String GIT_DATE = "2021-09-01T14:50:50Z";
-	public static final String BUILD_DATE = "2021-09-01T14:53:33Z";
-	public static final long BUILD_UNIX_TIME = 1630508013175L;
+	public static final String VERSION = "2.4-6aaf11d-1212-02-09-2021";
+	public static final int GIT_REVISION = 130;
+	public static final String GIT_SHA = "6aaf11dd86f177134008b746c93e9c98d61b3d34";
+	public static final String GIT_DATE = "2021-09-01T20:35:55Z";
+	public static final String BUILD_DATE = "2021-09-02T00:12:30Z";
+	public static final long BUILD_UNIX_TIME = 1630541550884L;
 }
